6a34d9b2b888c42fabbfacfa2d44c28a07fbcae0
samgolik <goliksam@gmail.com>
2024-02-17 12:39:43 -0500

    added login functionality

--- project-v/src/App.js
+++ project-v/src/App.js
@@ -1,19 +1,44 @@
-import './spotify.js';
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src='https://static.wixstatic.com/media/788444_a8de85da68974fe3b059bd851e1f386d~mv2.png/v1/fill/w_560,h_264,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Scaleable-Design-Files-and-Logo.png' className="App-logo" alt="logo" />
-        <p>
-          PROJECT V 
-        </p>
+  const CLIENT_ID = "2c2ff72235344ef3be61c2e425f770ac"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
 
-      </header>
-    </div>
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+      const hash = window.location.hash
+      let token = window.localStorage.getItem("token")
+
+      if (!token && hash) {
+          token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+          window.location.hash = ""
+          window.localStorage.setItem("token", token)
+      }
+
+      setToken(token)
+
+  }, [])
+
+  const logout = () => {
+      setToken("")
+      window.localStorage.removeItem("token")
+  }
+
+  return (
+      <div className="App">
+          <header className="App-header">
+              <h1>Spotify React</h1>
+              {!token ?
+                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                      to Spotify</a>
+                  : <button onClick={logout}>Logout</button>}
+          </header>
+      </div>
   );
 }
 
 export default App;
-
--- project-v/src/spotify.js
+++ project-v/src/spotify.js
@@ -1,35 +1,49 @@
-import express from 'express';
+const axios = require('axios');
+const qs = require('qs');
 
-const querystring = require('node:querystring');
+const client_id = '2c2ff72235344ef3be61c2e425f770ac'; // Your client id
+const client_secret = 'ba23e0f0f1754487a84b54286a5319d1'; // Your secret
+const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
 
-const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const getAuth = async () => {
+  try{
+    //make post request to SPOTIFY API for access token, sending relavent info
+    const token_url = 'https://accounts.spotify.com/api/token';
+    const data = qs.stringify({'grant_type':'client_credentials'});
 
-var client_id = '2c2ff72235344ef3be61c2e425f770ac';
-var redirect_uri = 'http://localhost:8888/callback';
-
-var app = express();
-
-app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
-
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-});
-
-function generateRandomString(length) {
-    let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result;
+    const response = await axios.post(token_url, data, {
+      headers: { 
+        'Authorization': `Basic ${auth_token}`,
+        'Content-Type': 'application/x-www-form-urlencoded' 
+      }
+    })
+    //return access token
+    return response.data.access_token;
+    //console.log(response.data.access_token);   
+  }catch(error){
+    //on fail, log the error in console
+    console.log(error);
+  }
 }
+
+const getAudioFeatures_Track = async (track_id) => {
+  //request token using getAuth() function
+  const access_token = await getAuth();
+  //console.log(access_token);
+
+  const api_url = `https://api.spotify.com/v1/audio-features/${track_id}`;
+  //console.log(api_url);
+  try{
+    const response = await axios.get(api_url, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+    console.log(response.data);
+    return response.data;
+  }catch(error){
+    console.log(error);
+  }  
+};
+
+console.log(getAudioFeatures_Track('07A0whlnYwfWfLQy4qh3Tq'));
