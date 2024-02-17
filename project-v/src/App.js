4eb665f361c6280971dbdad86bb6fff1c1d40a86
samgolik <goliksam@gmail.com>
2024-02-17 11:33:28 -0500

    init react project

---
+++ project-v/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://static.wixstatic.com/media/788444_a8de85da68974fe3b059bd851e1f386d~mv2.png/v1/fill/w_560,h_264,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Scaleable-Design-Files-and-Logo.png' className="App-logo" alt="logo" />
+        <p>
+          PROJECT V 
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
